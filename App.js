d10a4a38f89a03fb6ca8ad606e0d1cab3b5fd380
Eszter Laura Jónás <eszter.jonaska@gmail.com>
2020-10-26 23:24:16 +0100

    Added react-native-paper and used some elements

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Appbar, DefaultTheme } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const _goBack = () => console.log('Went back');
+
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => console.log('Shown more');
+
+  return (
+      <PaperProvider theme={theme}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={_goBack} />
+          <Appbar.Content title="Mobil Academy" subtitle="helló levi" />
+          <Appbar.Action icon="magnify" onPress={_handleSearch} />
+          <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+        </Appbar.Header>
+
+        <Card>
+          <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+          <Card.Content>
+            <Title>Card title</Title>
+            <Paragraph>Ezt csak úgy csináltam tesztelésre</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
+      </PaperProvider>
+    );
+}
